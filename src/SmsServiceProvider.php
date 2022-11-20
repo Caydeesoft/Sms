5861ebc3b39049a5667d70dd023c7031a3f23973
Dennis Kiptugen <dennis@Denniss-MacBook-Pro.local>
2022-11-20 12:59:56 +0300

    initial commit

---
+++ src/SmsServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Caydeesoft\Sms;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsServiceProvider  extends ServiceProvider
+	{
+	    public function boot()
+	    	{
+
+	    	}
+	    public function register()
+	    	{
+	    		
+	    	}	
+	}
---
+++ src/libs/Safaricom.php
@@ -0,0 +1,228 @@
+<?php
+ namespace Caydeesoft\Sms\libs;
+
+class Safaricom implements SmsInterface
+	{
+		
+	    public function __construct()
+		    {
+		      
+				   
+		    }
+		    
+		public function token()
+			{
+
+			} 
+
+		public function getAccessTokens()
+			{
+				try
+					{
+						$curl = curl_init();
+						curl_setopt( $curl, CURLOPT_URL, $this->cfg->accessTokenUrl );
+						curl_setopt( $curl, CURLOPT_HTTPHEADER, array(
+																				'Content-Type:application/json',
+																				'X-Requested-With:XMLHttpRequest'
+																			) );
+
+						$curl_post_data =   array(
+													'username' => $this->apiUsername,
+													'password' => $this->apiPassword
+												);
+
+						$data_string    =   json_encode( $curl_post_data );
+
+						curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, false );
+						curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
+						curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+						curl_setopt( $curl, CURLOPT_POST, true );
+						curl_setopt( $curl, CURLOPT_POSTFIELDS, $data_string );
+
+						$curl_response = curl_exec( $curl );
+						if ( $curl_response )
+							{
+								$results = json_decode( $curl_response );
+
+								return $results->token;
+							}
+						else
+							{
+								return 'Curl error: ' . curl_error( $curl );
+							}
+					}
+				catch(Exception $e)
+					{
+						return $e->getMessage();
+					}
+			}
+		public function getRefreshToken($refreshToken)
+			{
+				try
+					{
+						$ch = curl_init();
+						$headers    =   array(
+												'Content-Type:application/json',
+												'X-Requested-With:XMLHttpRequest'
+											);
+						$body       =   json_encode([]);
+						curl_setopt( $ch, CURLOPT_URL, $this->cfg->refreshTokenUrl);
+						curl_setopt( $ch, CURLOPT_HTTPHEADER,   $headers);
+						curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
+						curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+						curl_setopt( $ch, CURLOPT_CUSTOMREQUEST, 'GET' );
+						curl_setopt( $ch, CURLOPT_POSTFIELDS, $body );
+						curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+						$result = curl_exec($ch);
+						curl_close($ch);
+						if ( $result )
+							{
+								$results = json_decode( $result );
+
+								return $results->token;
+							}
+						else
+							{
+								return 'Curl error: ' . curl_error( $ch );
+							}
+					}
+				catch(Exception $e)
+					{
+						return $e->getMessage();
+					}
+			}
+		public function curlRequest($url,$data)
+			{
+				try
+					{
+						$authorization = "X-Authorization: Bearer " . $this->getAccessTokens();
+						$ch = curl_init();
+						curl_setopt($ch, CURLOPT_URL, $url);
+						curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json', $authorization));
+						curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+						curl_setopt($ch, CURLOPT_POST, true);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+						$result = curl_exec($ch);
+						curl_close($ch);
+						return json_decode($result);
+					}
+				catch (Exception $e)
+					{
+						return array('error'=>$e->getMessage());
+					}
+			}
+		public function bulkSms($new_sdp_data)
+			{
+				$json_data  =   array(
+										"timeStamp" => time(),
+										"dataSet" => [
+														array(
+																"userName"          =>  $new_sdp_data['userName'],
+																"channel"           =>  "sms",
+																"packageId"         =>  $new_sdp_data['packageId'],
+																"oa"                =>  $new_sdp_data['oa'],
+																"msisdn"            =>  $new_sdp_data['msisdn'],
+																"message"           =>  $new_sdp_data['message'],
+																"uniqueId"          =>  $new_sdp_data['msgid'],
+																"actionResponseURL" =>  $this->cfg->sendSmsCallback
+															)
+													]
+									);
+
+				$data       =   json_encode($json_data);
+				echo $data;
+				$result     =   $this->curlRequest($this->cfg->BulkSendUrl,$data);
+				if ($result)
+					{
+						return $result;
+					}
+				return FALSE;
+			}
+		public function sendSms($dt)
+			{
+				
+				$data   =   array(
+									[ "name" => "LinkId"    , 'value' => $dt['linkid']    ],
+									[ 'name' => 'Msisdn'    , 'value' => $dt['phone']     ],
+									[ 'name' => 'OfferCode' , 'value' => $dt['offercode'] ],
+									[ 'name' => 'Content'   , 'value' => $dt['msg']       ],				
+					                [ 'name' => 'CpId'      , 'value' => $this->cpid 	  ]
+								);
+
+				$json_data  =   array(
+										"requestId"         => $dt['id'],
+										"channel"           => "APIGW",										
+										"operation"         => "SendSMS",
+										"requestParam"      =>  array("data" => $data)
+
+									);
+			
+				$result     =   $this->curlRequest($this->cfg->sendSmsUrl,json_encode($json_data));
+				
+				if ($result)
+					{
+						return $result;
+					}
+				return FALSE;
+				
+				
+			}
+		public function subscription($dt)
+			{
+				$data   =   array(
+									[ 'name' => 'OfferCode' , 'value' => $dt['offercode'] ],
+									[ 'name' => 'Msisdn'    , 'value' => $dt['phone']     ],
+									[ "name" => "Language"  , 'value' => 'English'    ],
+									[ 'name' => 'CpId'      , 'value' => $this->cpid      ]
+								);
+				$json_data  =   array(
+										"requestId"         =>  $dt['id'],
+										"requestTimeStamp"  =>  date('YmdHis'),
+										"channel"           =>  "SMS",
+										"operation"         =>  "ACTIVATE",
+										"requestParam"      =>  array("data" => $data)
+									);
+				return $this->curlRequest($this->cfg->subscriptionUrl,json_encode($json_data));
+			}
+		public function unsubscription($dt)
+			{
+				$data   =   array(
+									[ 'name' => 'OfferCode' , 'value' => $dt['offercode'] ],
+									[ 'name' => 'Msisdn'    , 'value' => $dt['phone']     ],
+									[ 'name' => 'CpId'      , 'value' => $this->cpid      ]
+								);
+				$json_data  =   array(
+										"requestId"         =>  $dt['id'],
+										"requestTimeStamp"  =>  date('YmdHis'),
+										"channel"           =>  "SMS",
+										"sourceAddress"     =>  $this->ci->input->ip_address(),
+										"operation"         =>  "DEACTIVATE",
+										"requestParam"      =>  array("data" => $data)
+									);
+				return $this->curlRequest($this->cfg->unSubscriptionUrl,json_encode($json_data));
+			}
+		public function cpNotification($dt,$additionaldata = NULL)
+			{
+				$data   =   array(
+									[ 'name' => 'OfferCode' , 'value' => $dt['offercode'] ],
+									[ 'name' => 'Msisdn'    , 'value' => $dt['phone']     ],
+									[ 'name' => 'Command'   , 'value' => $dt['command']   ]
+								);
+				$json_data  =   array(
+										"requestId"         =>  $dt['id'],
+										"requestTimeStamp"  =>  date('YmdHis'),
+										"operation"         =>  "CP_NOTIFICATION",
+										"requestParam"      =>  array("data" => $data)
+									);
+				if(is_array($additionaldata))
+					{
+						foreach($additionaldata as $key => $value)
+							{
+								$json_data["additionalData"][] = array( 'name' => $key , 'value' => $value );
+							}
+					}
+			}
+	}
